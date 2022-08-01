d753e438a89a1affc9394a33cf2a41474be74cea
Morrdee <110430024+Morrdee@users.noreply.github.com>
2022-08-01 23:06:53 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Hustlers Uni</title>
+        <meta name="description" content="Hustlers University" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Hustlers Uni</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
